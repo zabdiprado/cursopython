75567959495d4a1d0146babd021afc3fa7196278
Zabdi Palomares Prado <zabdipalomaresprado@MacBook-Air-de-Zabdi.local>
2024-08-18 17:41:24 -0600

    primer commit

---
+++ operadores.py
@@ -0,0 +1,28 @@
+# operadores aritméticos
+
+print(10 + 8) # suma
+print(23 - 7) # resta
+print(8 * 5) # repetición
+print(15 / 2) # división
+print(11 % 4) # modulo
+print(11 // 4) # division de piso
+print(2 ** 3) # poteniación
+
+#operadores de cadena de texto
+
+print("Hola" + "mundo") # concatenación
+print("Hola" * 3) # repetición
+print("Hola" + "mundo" * 3)
+
+# operadores de comparación
+
+print("Hola" == "hola") # igual que
+print("Hola" != "hola") # distinto que
+print(3 < 11) # mayor que, menor que
+print(11 >= 10) # mayor o igual
+print(10 <= 10) # menor o igual
+
+# operadores de existencias
+
+print("ola" in "hola")
+print("ola" not in "hola")
---
+++ variables.py
@@ -0,0 +1,25 @@
+# Aqui hay una variable de tipo entero
+numero= 147 # Esta linea sirve para imprimir la ariable número
+print(numero)
+print(type(numero))
+
+# Redefinimos la variable número
+numero = 147.5
+print(numero)
+print(type(numero))
+
+complejo = 1 + 2j
+print(complejo)
+print(type(complejo))
+
+palabra = "Hola"
+print(palabra)
+print(type(palabra))
+
+bandera = True
+print(bandera)
+print(type(bandera))
+
+vacio = None
+print(vacio)
+print(type(vacio))
